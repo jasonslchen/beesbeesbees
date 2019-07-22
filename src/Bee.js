cbc6e04cb5ad7ac1aa8ef03953316f3083c6b572
Jason Chen <jason.sl.chen@gmail.com>
2019-07-22 12:12:36 -0700

    complete beesbeesbees repo

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  Grub.call(this);
+
+  this.color = 'yellow';
+
+  this.age = 5;
+
+  this.job = 'keep on growing';
+
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,26 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+
+  this.age = 10;
+
+  this.honeyPot = 0;
+
+  this.job = 'make honey';
+
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+
+
 
